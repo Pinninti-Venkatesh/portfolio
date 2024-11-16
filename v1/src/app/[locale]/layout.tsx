91aa56d81eafce093b84b6cf5d1bd969c0f89716
venky <pvenkatesh483@gmail.com>
2024-11-16 21:36:57 +0530

    added versel analytics

--- v1/src/app/[locale]/layout.tsx
+++ v1/src/app/[locale]/layout.tsx
@@ -92,6 +92,7 @@ export default async function RootLayout({
 	const messages = await getMessages();
 	return (
 		<NextIntlClientProvider messages={messages}>
+			<Analytics/>
 			<Flex
 				as="html" lang="en"
 				background="page"
